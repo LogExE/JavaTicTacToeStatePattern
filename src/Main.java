61e78bc749c4d1a2d997edfa9ef62a5414ab0bdd
LogExE <awesome149712@gmail.com>
2023-03-28 21:38:48 +0400

    short cmds + more checks

--- src/Main.java
+++ src/Main.java
@@ -11,13 +11,13 @@ public class Main {
             String[] com = inp.split(" ");
             if (com.length == 0)
                 continue;
-            if (com[0].equals("place"))
+            if ((com[0].equals("place") || com[0].equals("p")) && com.length == 3)
                 game.place(Integer.parseInt(com[1]), Integer.parseInt(com[2]));
-            else if (com[0].equals("status"))
+            else if ((com[0].equals("status") || com[0].equals("s")) && com.length == 1)
                 System.out.println(game.getStatus());
-            else if (com[0].equals("reset"))
+            else if ((com[0].equals("reset") || com[0].equals("r"))  && com.length == 1)
                 game.reset();
-            else if (com[0].equals("exit"))
+            else if ((com[0].equals("exit") || com[0].equals("e"))  && com.length == 1)
                 break;
             else System.out.println("Available commands are:\nplace r c\nstatus\nreset\nexit");
         }
